1435e18a864a2bfdd4eb885f0d913dada781dbc9
KudoCC <cangmuma@gmail.com>
2016-06-18 22:04:14 +0800

    add

---
+++ compare.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+def Test1(rootDir, preLen):
+    list = []
+    list_dirs = os.walk(rootDir)
+    for root, dirs, files in list_dirs:
+        for d in dirs:
+            str = os.path.join(root, d)
+            str = str[preLen:]
+            list.append(str)
+        for f in files:
+            str = os.path.join(root, f)
+            str = str[preLen:]
+            list.append(str)
+    return list
+
+if len(sys.argv) != 3:
+    print "argv should be 3 , python file and one for old directory , the other for new directory!"
+    sys.exit(0)
+fileOld = sys.argv[1]
+preLenOld = len(fileOld)
+fileNew = sys.argv[2]
+preLenNew = len(fileNew)
+listOld = Test1(fileOld, preLenOld)
+listNew = Test1(fileNew, preLenNew)
+
+setOld = set(listOld)
+setNew = set(listNew)
+
+print "old list size is", len(listOld)
+print "new list size is", len(listNew)
+
+for i in range(1, len(listOld)):
+    if listOld[i] not in setNew:
+        print listOld[i] ,"is removed"
+for i in range(1, len(listNew)):
+    if listNew[i] not in setOld:
+        print listNew[i] ,"is added"
+    else:
+        filePath1 = fileOld + listNew[i]
+        filePath2 = fileNew + listNew[i]
+        fSize1 = os.path.getsize(filePath1)
+        fSize2 = os.path.getsize(filePath2)
+        if fSize1 != fSize2:
+            print listNew[i] ,"is modifyed"
+
